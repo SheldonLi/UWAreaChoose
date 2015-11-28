be9d0b99c9bf214752601c2a03153adef5693737
li shaoyou <sheldonli@uworks.cc>
2015-11-28 18:34:37 +0800

    项目代码导入

--- UWAreaChoose/ViewController.m
+++ UWAreaChoose/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UWChooseAreaViewController.h"
 
 @interface ViewController ()
 
@@ -22,6 +23,28 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (IBAction)areaChooseClick:(id)sender {
+    
+    //  创建城市选择控制器
+    UWChooseAreaViewController *chooseVC = [[UWChooseAreaViewController alloc] init];
+    //  修改标题
+    chooseVC.titleString = @"城市选择";
+    //  最大选择级别
+    chooseVC.maxRegionLevel = 3;
+    //  完成回调：获取城市ID数组
+    chooseVC.completionBlockWithCityIdArray = ^(NSArray *areaModelArray) {
+        NSLog(@"%@", areaModelArray);
+    };
+    //  完成回调：获取城市名数组
+    chooseVC.completionBlockWithCityNameArray = ^(NSArray *areaModelArray) {
+        NSLog(@"%@", areaModelArray);
+    };
+    //  弹出控制器
+    [self.navigationController pushViewController:chooseVC animated:YES];
+    
 }
 
 @end
